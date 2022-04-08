553f66215b88027f05df68230123d7e32a86574b
xvferdy <xv.ferdy@gmail.com>
2022-04-08 16:37:30 +0800

    ✨ [feature]: floating bottom navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
+  const handleActiveNav = (urlFragment) => {
+    setActiveNav(urlFragment);
+  };
   return (
     <nav className="nav">
-      <a href="#" className="active">
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={() => handleActiveNav("#")}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        className={activeNav === "#about" ? "active" : ""}
+        onClick={() => handleActiveNav("#about")}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        className={activeNav === "#experience" ? "active" : ""}
+        onClick={() => handleActiveNav("#experience")}
+      >
         <BiBook />
       </a>
-      <a href="#service">
+      <a
+        href="#service"
+        className={activeNav === "#service" ? "active" : ""}
+        onClick={() => handleActiveNav("#service")}
+      >
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        className={activeNav === "#contact" ? "active" : ""}
+        onClick={() => handleActiveNav("#contact")}
+      >
         <BiMessageSquareDetail />
       </a>
     </nav>
