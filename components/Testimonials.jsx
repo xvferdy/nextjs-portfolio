d610b521f0f7b5392f2146750cd1f331e3636c36
xvferdy <xv.ferdy@gmail.com>
2022-04-09 15:33:32 +0800

    🚧 [WIP]: <Testimonials /> JSX

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -3,7 +3,58 @@ import React from "react";
 function Testimonials() {
   return (
     <section id="testimonials" className="testimonials">
-      Testimonials
+      <h5>Review From Clients </h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="/assets/avatar1-ori.jpg" alt="Avatar" />
+            <h5 className="client__name">Naomi Yahukimo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+              hic fugiat doloremque, possimus quas obcaecati maiores, non
+              perspiciatis saepe reprehenderit eveniet. Laboriosam voluptatibus
+              hic ex. Beatae fuga maxime odit odio?
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="/assets/avatar1-ori.jpg" alt="Avatar" />
+            <h5 className="client__name">Naomi Yahukimo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+              hic fugiat doloremque, possimus quas obcaecati maiores, non
+              perspiciatis saepe reprehenderit eveniet. Laboriosam voluptatibus
+              hic ex. Beatae fuga maxime odit odio?
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="/assets/avatar1-ori.jpg" alt="Avatar" />
+            <h5 className="client__name">Naomi Yahukimo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+              hic fugiat doloremque, possimus quas obcaecati maiores, non
+              perspiciatis saepe reprehenderit eveniet. Laboriosam voluptatibus
+              hic ex. Beatae fuga maxime odit odio?
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="/assets/avatar1-ori.jpg" alt="Avatar" />
+            <h5 className="client__name">Naomi Yahukimo</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+              hic fugiat doloremque, possimus quas obcaecati maiores, non
+              perspiciatis saepe reprehenderit eveniet. Laboriosam voluptatibus
+              hic ex. Beatae fuga maxime odit odio?
+            </small>
+          </div>
+        </article>
+      </div>
     </section>
   );
 }
